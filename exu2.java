f8333c064bf4200d64ef926f529d7b5ecb53e3af
Damian Azarael Zazueta Rangel <damianzazueta306@gmail.com>
2026-05-29 08:18:49 -0600

    1

--- exu2.java
+++ exu2.java
@@ -13,7 +13,7 @@ public class exu2 {
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Sistema logistico Empacadora Oro verde");
-        System.out.println("/nIngrese el total de aguacates cosechados hoy");
+        System.out.println("\nIngrese el total de aguacates cosechados hoy");
         System.out.println("");
         long totalAguacates = entreda.nextlong();
         System.out.println("Procesando distribucion");
@@ -27,16 +27,49 @@ public class exu2 {
         long tarimasSobrantes = obtenerResiduo(totalTarimas,TarimasPC);
 
         double ingresosContenedores = calcularIngreso(contenedores,PrecioConte);
-        double ingresosTarimas = calcularIngreso(tarimasSobrantes, )
-        
+        double ingresosTarimas = calcularIngreso(tarimasSobrantes, PrecioTarima);
+        double ingresosRejas = calcularIngreso(rejasSobrantes,PrecioReja);
+        double ingresosAguacates = calcularIngreso(aguacatesSueltos, PrecioAguacates);
+
+        double ingresoBrutoTotal = ingresosContenedores + ingresosTarimas + ingresosRejas + ingresosAguacates;
+        double impuestoAduanero = calcularImpuesto(ingresosContenedores,ImpuestoTasa);
+        double ingresoNetoReal = ingresoBrutoTotal -impuestoAduanero;
+
+        System.out.println("\n Reporte de embalaje")
+        System.out.println("Contenedores completos para exportar:" + contenedores);
+        System.out.println("Tarimas sobrantes:"+tarimasSobrantes);
+        System.out.println("Rejas sobrantes:" + rejasSobrantes);
+        System.out.println("Aguacates sueltos:" + aguacatesSueltos);
+
+        System.out.println("\n desglose financiero");
+        System.out.printf("Ingresos por contenedores $%,.2f MXN\n", ingresosContenedores);
+        System.out.printf("Ingresos por tarimas: $%,.2f MXN\n" ingresosTarimas);
+        System.out.printf("Ingresos por rejas $%,.2f MXN\n" ingresosRejas);
+        System.out.printf("Ingresos por aguacate suelto $%,.2f MXN\n" ingresosAguacates);
+
+         System.out.printf("\n ingreso bruto total : $%,.2f MXN\n" ingresoBrutoTotal);
+         System.out.printf("impuesto aduanero(5%): -$%,.2f MXN (solo exp)\n", impuestoAduanero;)
+         System.out.printf("ingreso neto real $%,.2f MXN\n:", ingresoNetoReal);
+         System.out.printf("Reporte generado exitosamente:");
+
+         entrada.close();
 
 
 
     }
+    public static long obtenerCociente(long dividiendo, int divisor){
+        return dividiendo / divisor;
+    }
+    public static long obtenerResiduo(long dividiendo, int divisor){
+        return dividiendo % divisor; }
 
+        public static double calcularIngreso (long cantidad, double preciounitario){
+        return cantidad * preciounitario; }
 
+        public static double calcularImpuesto(double baseImponible, double tasa) {
+        return baseImponible * tasa;
+
+       
         
-
-
     }
         
