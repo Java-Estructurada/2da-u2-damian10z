785e0fa0120bed89e237f2b29aa181cf13bfa351
Damian Azarael Zazueta Rangel <damianzazueta306@gmail.com>
2026-05-29 07:44:29 -0600

    1

---
+++ exu2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class exu2 {
+    public static final int AguacatesPR=40;
+    public static final int RejasPT =24; 
+    public static final int TarimasPC = 18;
+    
+    public static final double PrecioConte = 150,000.;
+    public static final double PrecioTarima = 7500;
+    public static final double PrecioReja = 280;
+    public static final double ImpuestoTasa = 0.05;
+
+    public static void main (String[]args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Sistema logistico Empacadora Oro verde")
+        System.out.println("/nIngrese")
+        System.out.println("")
+    }
+
+
+        
+
+
+    }
+        
